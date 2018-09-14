1927736162b8f1dc20b3b9ec9635b202983365cb
Vish Desai <shadyvd@hotmail.com>
2018-09-14 14:05:43 +0530

    First-cut integration tests for the Profile Feature

---
+++ test/features/profile.spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const chai = require('chai'); // eslint-disable-line node/no-unpublished-require
+const chaiHttp = require('chai-http'); // eslint-disable-line node/no-unpublished-require
+
+chai.use(chaiHttp);
+
+describe('Profile Feature Test Cases', function() {
+	const agent = chai.request.agent('http://localhost:9100');
+	const expect = chai.expect;
+
+	chai.should();
+
+	it('Should not return the User details if not logged in', function(done) {
+		agent
+			.get('/profile')
+			.end((err, response) => {
+				expect(response).to.have.status(500);
+				done(err);
+			});
+	});
+});
